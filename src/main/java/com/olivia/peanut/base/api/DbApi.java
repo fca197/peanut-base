95ad72f007156ceeb50207dc6876b5f7b5b7cad6
wangbao <197235339@qq.com>
2025-03-22 20:46:05 +0800

    增加db还原接口

---
+++ src/main/java/com/olivia/peanut/base/api/DbApi.java
@@ -0,0 +1,12 @@
+package com.olivia.peanut.base.api;
+
+import com.olivia.peanut.base.api.entity.db.DbResetReq;
+import com.olivia.peanut.base.api.entity.db.DbResetRes;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface DbApi {
+  @PostMapping("/db/reset")
+  DbResetRes dbReset(@RequestBody @Valid DbResetReq req);
+}
